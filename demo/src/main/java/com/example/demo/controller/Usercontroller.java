d515b79690062c8bd6348952b63de89f3e02d160
kewiibird <kewiibird@gmail.com>
2026-04-08 15:53:24 +0900

    260408 kakao 소셜 로그인

--- demo/src/main/java/com/example/demo/controller/Usercontroller.java
+++ demo/src/main/java/com/example/demo/controller/Usercontroller.java
@@ -3,17 +3,28 @@ package com.example.demo.controller;
 import java.io.File;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.dao.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,8 +37,17 @@ public class Usercontroller {
 	@Autowired
 	UserService userService;
 	
+	@Value("${client_id}")
+	private String client_id;
+
+    @Value("${redirect_uri}")
+    private String redirect_uri;
+
+
 	@RequestMapping("/login.do")
 	public String login(Model model) throws Exception{
+		String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+        model.addAttribute("location", location);
 		return "/user/login";
 	}
 	@RequestMapping(value = "/login.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
@@ -38,6 +58,57 @@ public class Usercontroller {
 		resultMap = userService.login(map);
 		
 		return new Gson().toJson(resultMap); 
+	}
+	
+	@RequestMapping("/main.do")
+	public String main(Model model) throws Exception{
+		return "/main";
+	}
+	//kakao로그인
+	@RequestMapping(value = "/kakao.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String kakao(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+	        String tokenUrl = "https://kauth.kakao.com/oauth/token";
+
+	        RestTemplate restTemplate = new RestTemplate();
+	        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+	        params.add("grant_type", "authorization_code");
+	        params.add("client_id", client_id);
+	        params.add("redirect_uri", redirect_uri);
+	        params.add("code", (String)map.get("code")); //map안에있는 code꺼낸
+
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+	        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+	        ResponseEntity<Map> response = restTemplate.postForEntity(tokenUrl, request, Map.class);
+
+	        Map<String, Object> responseBody = response.getBody();
+	        System.out.println(responseBody);
+	        resultMap = (HashMap<String, Object>)getUserInfo((String)responseBody.get("access_token"));
+	        
+		return new Gson().toJson(resultMap); 
+		
+	}
+	
+	private Map<String, Object> getUserInfo(String accessToken) {
+	    String userInfoUrl = "https://kapi.kakao.com/v2/user/me";
+
+	    RestTemplate restTemplate = new RestTemplate();
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setBearerAuth(accessToken);
+	    HttpEntity<String> entity = new HttpEntity<>(headers);
+
+	    ResponseEntity<String> response = restTemplate.exchange(userInfoUrl, HttpMethod.GET, entity, String.class);
+
+	    try {
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        return objectMapper.readValue(response.getBody(), Map.class);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return null; // 예외 발생 시 null 반환
+	    }
 	}
 	
 	@RequestMapping("/join.do")
